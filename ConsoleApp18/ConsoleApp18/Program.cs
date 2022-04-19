4e4d241468281402beb02484a2309557176bef91
zeynallisahile <tu7dfbyuu@code.edu.az>
2022-04-19 12:51:47 +0400

    Task Completed

---
+++ ConsoleApp18/ConsoleApp18/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace ConsoleApp18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+        static void AddPost(string title, string content)
+        {
+            string Connection = @"Server=C:\Users\acer\Desktop\Sahilə;Blog;Trusted_Connection=True";
+            Console.WriteLine("Title");
+            string title = Console.ReadLine();
+            Console.WriteLine("Content");
+            string content = Console.ReadLine();
+            using (SqlConnection con = new SqlConnection(Connection))
+            {
+                con.Open();
+                string query = $"INSERT INTO Blog(Title, Content) VALUES('{title}','{content}')";
+                SqlCommand cmd = new SqlCommand(query, con);
+
+                cmd.ExecuteNonQuery();
+            }
+
+           }
+        }
+    }
+
